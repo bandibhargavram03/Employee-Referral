baeaf525389557f4b5c5e846955ad8e4d1e4c909
Bhargav Ram Bandi <bandibhargav1999@gmail.com>
2023-11-18 22:28:05 +0530

    first commit

---
+++ src/main/java/com/bhargav/EmployeeReferralApplication.java
@@ -0,0 +1,13 @@
+package com.bhargav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReferralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReferralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhargav/EmployeeReferralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhargav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReferralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
